c43e42e4f0368bec8e4144f97656112b7532cd98
venkateswaranprasad <69619655+venkateswaranprasad@users.noreply.github.com>
2026-02-16 13:23:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2704,6 +2704,15 @@ window.addEventListener("load", async () => {
   
 });
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/badminton-match-generator/sw.js")
+      .then(reg => console.log("✅ SW registered", reg.scope))
+      .catch(err => console.error("❌ SW registration failed", err));
+  });
+}
+
 /***********************
  * EXPOSE FUNCTIONS for onclick
  ***********************/
